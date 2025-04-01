1ae5687578ac9b2608f0571a6fb66f73cfa74d49
nepeat@devbox <nepeat@gmail.com>
2025-04-01 20:16:07 +0000

    refactor stats to use redis hashes, consume stream to build stats

---
+++ field/consumers/counter.py
@@ -0,0 +1,61 @@
+import redis
+import json
+import asyncio
+
+from field.consumers import ConsumerBase
+
+
+class CounterConsumer(ConsumerBase):
+    CONSUMER_NAME = "consumer:counter"
+
+    async def main(self):
+        # Create a consumer group
+        try:
+            await self.redis.xgroup_create(
+                "field:stream",
+                self.CONSUMER_NAME,
+                "0",
+                mkstream=False,
+            )
+        except redis.exceptions.ResponseError as e:
+            if e.args[0] != "BUSYGROUP Consumer Group name already exists":
+                raise e
+
+        # iterate through the redis stream
+        while True:
+            # try to get pending messages first
+            redis_messages = await self.redis.xreadgroup(
+                self.CONSUMER_NAME,
+                self.CONSUMER_NAME + ":0",
+                {"field:stream": "0"},
+                count=5,
+            )
+
+            # try get 5 unclaimed messages for the group
+            if not redis_messages[0][1]:
+                redis_messages = await self.redis.xreadgroup(
+                    self.CONSUMER_NAME,
+                    self.CONSUMER_NAME + ":0",
+                    {"field:stream": ">"},
+                    count=5,
+                )
+
+            if not redis_messages[0][1]:
+                print("No messages, sleeping")
+                await asyncio.sleep(1)
+                continue
+
+            # process the messages
+            stream_name, messages = redis_messages[0]
+
+            for message_id, message in messages:
+                payload_wrapper = json.loads(message["message"])["subscribe"]
+                payload = payload_wrapper["data"]["payload"]
+                msg_type = payload["msg"]["type"]
+                await self.redis.hincrby("field:message_counter", msg_type, 1)
+                await self.redis.xack("field:stream", self.CONSUMER_NAME, message_id)
+
+
+if __name__ == "__main__":
+    consumer = CounterConsumer()
+    asyncio.run(consumer.main())
--- util/stats.py
+++ util/stats.py
@@ -1,5 +1,4 @@
-import math
-import json
+import ujson as json
 from collections import defaultdict
 from field.constants import MAX_MESSAGES
 from field import connections
@@ -9,27 +8,21 @@ redis = connections.new_redis()
 
 # set counters
 type_count = defaultdict(lambda: 0)
-total_bytes = 0
 
 # get message count
 msgcount = redis.xlen("field:stream")
 
-# message iteration
-for message_id, redis_message in redis.xrange("field:stream"):
-    message = json.loads(redis_message["message"])
-    payload = message["subscribe"]
-
-    # stat counting
-    message_type = payload["data"]["payload"]["msg"]["type"]
-    type_count[message_type] += 1
-    total_bytes += len(redis_message["message"])
-
 stats = {
-    "message_types": type_count,
+    "message_types": redis.hgetall("field:message_counter"),
     "message_count": msgcount,
     "remaining_messages": MAX_MESSAGES - msgcount,
-    "message_avg_size": math.ceil(total_bytes / msgcount),
-    "total_bytes": total_bytes,
+    "consumers": {},
 }
+
+# add consumer group info
+for group in redis.xinfo_groups("field:stream"):
+    cloned_group = group.copy()
+    cloned_group.pop("name")
+    stats["consumers"][group["name"]] = cloned_group
 
 print(json.dumps(stats, indent=4))
