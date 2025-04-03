a77a1c4a5bc8f58da00648e045f9c2d589e0412f
nepeat@devbox <nepeat@gmail.com>
2025-04-03 01:34:47 +0000

    so it turns out autocommit does not work

--- _oneoff_migrations/002_backfill_hasfile.py
+++ _oneoff_migrations/002_backfill_hasfile.py
@@ -4,24 +4,32 @@ import asyncio
 import glob
 
 
-from sqlalchemy.sql import select
+from sqlalchemy.sql import select, func
 
-from field.model import sm, sm_autocommit, FieldPartitionUpdate
+
+from field.model import sm, FieldPartitionUpdate
 
 
 @lru_cache(maxsize=128)
 def get_files(path):
-    print("Listing files in", path)
+    print("\rListing files in", path)
     files = glob.glob(path + "/*")
     return files
 
 
 async def main():
     db = sm()
-    db_update = sm_autocommit()
+    db_update = sm()
     partitions = await db.stream_scalars(
         select(FieldPartitionUpdate).filter(FieldPartitionUpdate.has_file == False)  # noqa: E712
     )
+    partitions_count = await db.scalar(
+        select(func.count())
+        .select_from(FieldPartitionUpdate)
+        .filter(FieldPartitionUpdate.has_file == False)  # noqa: E712
+    )
+
+    i = 0
 
     async for partition in partitions.yield_per(1024 * 10):
         # update the object's db session
@@ -46,6 +54,14 @@ async def main():
             partition.has_file = True
         else:
             print("Did not find file for ", partition.url)
+
+        i += 1
+        if i % 128 == 0:
+            remaining = partitions_count - i
+            print(f"\r{i:,} processed, {remaining:,} remaining", end="", flush=True)
+            await db_update.commit()
+
+    await db_update.commit()
 
 
 if __name__ == "__main__":
